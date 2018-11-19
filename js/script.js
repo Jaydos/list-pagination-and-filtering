227f416e615b9202eb1193cb9c6c0f0f92209e46
Jaydos <jaydenoxley00@gmail.com>
2018-11-19 19:44:57 +1000

    Completed project with exceeds expectation functionality added

--- js/script.js
+++ js/script.js
@@ -2,49 +2,47 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+//I chose to put all functions in a seperate script to maintain readabiliy
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// Declare global variables
+const numPerPage = 10;
+const mainPage = document.querySelector('.page');
+const header = document.querySelector('.page-header');
+const ul = document.querySelector('.student-list');
+const studentList = ul.children;
+let filter = '';
+const paginationDiv = document.createElement('div');
+paginationDiv.className = 'pagination';
 
+// Show first ten students by default and create pagination links, as well as add search bar.
+showPage(studentList, 1); 
+appendPageLinks(studentList);
+createSearchBar();
 
+// Input event listener on search bar for live filtering of students.
+document.querySelector('input').addEventListener('input', (e) => {
+    // Empty pagination div so newly created pagination links are not stacked on top of each other.
+    clearPaginationDiv();
+    // Update filter value to be used when generating a filtered list.
+    filter = e.target.value;
+    //Store the results of the generateFilteredList function in a variable.
+    let filteredList = generateFilteredList(studentList);
+    // Re-display students, passing the newly created filtered list as argument.
+    showPage(filteredList, 1); 
+    appendPageLinks(filteredList);
+});
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// Click event listener on pagination div.
+document.querySelector('.pagination').addEventListener('click', (e) => {
+    let activeLink = document.querySelector('.active');
+    // Check if target was an anchor tag.
+    if(e.target.tagName === 'A'){
+        if(activeLink){
+            activeLink.classList.remove('active');
+        }
+        e.target.className = 'active';
+        // Show corresponding group of filtered students based on textContent of the clicked anchor tag.
+        showPage(generateFilteredList(studentList), e.target.textContent);
+    }
+});
